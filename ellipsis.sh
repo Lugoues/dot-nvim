e6e8005465080da29050e5975fea89ccc7ca7bd5
Peter Brunner <pbrunner@gmail.com>
2016-11-05 12:12:30 -0400

    Install plugins when installed nvim bundle

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,38 +2,21 @@
 #
 # lugoues/nvim ellipsis package
 
-# The following hooks can be defined to customize behavior of your package:
-#pkg.install() {
-#  fs.link_rfile $HOME/.config
-#}
-
-# pkg.push() {
-#     git.push
-# }
-
-# pkg.pull() {
-#     git.pull
-# }
-
-# pkg.installed() {
-#     git.status
-# }
-#
-# pkg.status() {
-#     git.diffstat
-# }
-
 pkg.install() {
-    if ! utils.cmd_exists nvim; then
-        log.fail "Unmet dependency 'nvim'"
-        return 1
-    fi
+  if ! utils.cmd_exists nvim; then
+    log.fail "Unmet dependency 'nvim'"
+    return 1
+  fi
 }
 
 pkg.link() {
   # Link into .config
   mkdir -p "$ELLIPSIS_HOME/.config"
   fs.link_rfile "$PKG_PATH" "$ELLIPSIS_HOME/.config/nvim"
+
+  # Install Plugins
+  echo "Installing nvim Plugins..."
+  nvim +PlugInstall +qall --headless
 }
 
 pkg.unlink() {
